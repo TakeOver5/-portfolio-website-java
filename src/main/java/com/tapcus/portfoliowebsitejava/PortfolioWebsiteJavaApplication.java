547fadaeb580aebbcd2e370e4a8c566c2715428d
school <tapirusindicus5@gmail.com>
2022-08-24 09:53:17 +0800

    init

---
+++ src/main/java/com/tapcus/portfoliowebsitejava/PortfolioWebsiteJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tapcus.portfoliowebsitejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioWebsiteJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioWebsiteJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tapcus/portfoliowebsitejava/PortfolioWebsiteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapcus.portfoliowebsitejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
